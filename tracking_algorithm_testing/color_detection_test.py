d3c8684b379e012727cbb04795f6afae7b9f2533
ahpinder <42349096+ahpinder@users.noreply.github.com>
2024-10-12 19:08:52 -0400

    Added color tracking and color graph display
    Code now displays a graph of the hue of all tracked points that are present at the end of the video feed

--- tracking_algorithm_testing/color_detection_test.py
+++ tracking_algorithm_testing/color_detection_test.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 cap = cv2.VideoCapture('output.avi')
 bparams = cv2.SimpleBlobDetector_Params()
@@ -22,6 +23,9 @@ sift = cv2.SIFT_create()
 seenmatches = []
 matchstats = []
 matchage = []
+trackedcolors = []
+
+showcameraview = False
 
 while(1):
     ret, img = cap.read()
@@ -31,7 +35,6 @@ while(1):
     rev = 255 - gray
     keypoints = bdet.detect(rev)
     descriptors = np.array([point.pt for point in keypoints], np.float32)
-    #print(descriptors)
     blank = np.zeros((1, 1))
     if not firsttime:
         matches = matcher.match(descriptors, lastdescriptors)
@@ -39,6 +42,12 @@ while(1):
         for match in matches:
             lastloc = lastpoints[match.trainIdx].pt
             curloc = keypoints[match.queryIdx].pt
+            currad = round(keypoints[match.queryIdx].size / 2)
+            cx = round(curloc[1])
+            cy = round(curloc[0])
+            curpatch = img[cx-currad:cx+currad,cy-currad:cy+currad]
+            colavg = np.mean(curpatch, axis=(0,1))
+            hue = float(colavg[0])
             try:
                 matchidx = seenmatches.index(lastpoints[match.trainIdx])
             except:
@@ -46,28 +55,39 @@ while(1):
             if matchidx != -1:
                 matchstats[matchidx] += 1
                 seenmatches[matchidx] = keypoints[match.queryIdx]
+                trackedcolors[matchidx].append(hue)
             else:
                 seenmatches.append(keypoints[match.queryIdx])
                 matchstats.append(2)
                 matchage.append(2)
-            #print(str(lastloc) + " " + str(curloc))
+                trackedcolors.append([hue])
             goodpts.append(keypoints[match.queryIdx])
-            blobs = cv2.drawKeypoints(img, goodpts, blank, (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         matchage = [age + 1 for age in matchage]
         for i in range(len(matchage))[::-1]:
             if matchage[i] > matchstats[i] + 1:
                 matchstats.pop(i)
                 seenmatches.pop(i)
                 matchage.pop(i)
-        print(matchstats)
-    else:
+                trackedcolors.pop(i)
+        print(len(trackedcolors[0]))
+        if showcameraview:
+            blobs = cv2.drawKeypoints(img, seenmatches, blank, (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    elif showcameraview:
         blobs = cv2.drawKeypoints(img, keypoints, blank, (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imshow('image',blobs)
-    x = cv2.waitKey(33)
-    if x == 27:
-        break
+    if showcameraview:
+        cv2.imshow('image',blobs)
+        x = cv2.waitKey(33)
+        if x == 27:
+            break
     lastpoints = keypoints
     lastdescriptors = descriptors
     firsttime = False
 
 cap.release()
+cv2.destroyAllWindows()
+
+npcolors = np.array(trackedcolors)
+
+for dot in npcolors:
+    plt.plot(dot)
+plt.show()
