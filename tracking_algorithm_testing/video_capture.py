8a8da4ead28cffd2d6241ddeac163b5c2eb4aa89
LilG0053 <140113715+LilG0053@users.noreply.github.com>
2024-10-15 16:41:45 -0400

    Update video_capture.py

--- tracking_algorithm_testing/video_capture.py
+++ tracking_algorithm_testing/video_capture.py
@@ -3,13 +3,10 @@ import numpy as np
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_EXPOSURE, -12)
-detector = cv2.SimpleBlobDetector()
 ret,img = cap.read()
 writer = cv2.VideoWriter('output.avi',cv2.VideoWriter.fourcc('M','J','P','G'),30,(640,480))
 while(1):
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    blobimg = 255 - gray
     cv2.imshow('image',img)
     x = cv2.waitKey(1)
     if x == 27:
