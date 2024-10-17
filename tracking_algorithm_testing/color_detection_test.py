a1f4815c3f2ff7ff7dca7d18af13db41d6b7f67b
ahpinder <42349096+ahpinder@users.noreply.github.com>
2024-10-17 15:05:17 -0400

    Added more visualization + saturation
    Added saturation variable as well as hue variable, and created a real-time hue-saturation visualization in an OpenCV window

--- tracking_algorithm_testing/color_detection_test.py
+++ tracking_algorithm_testing/color_detection_test.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from math import sin, cos, radians
 
 cap = cv2.VideoCapture('output.avi')
 bparams = cv2.SimpleBlobDetector_Params()
@@ -22,9 +23,16 @@ matcher = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
 seenmatches = []
 matchstats = []
 matchage = []
-trackedcolors = []
+trackedhue = []
+trackedsat = []
 
-showcameraview = False
+demorad = 256
+democolors = ((255,255,0),(255,0,255),(0,255,255))
+demoimg = np.zeros((demorad*2,demorad*2,3),np.uint8)
+decay_factor = 50 # parameter for hue-saturation visualization
+
+showcameraview = True
+showdemoview = True
 
 while(1):
     ret, img = cap.read()
@@ -48,6 +56,7 @@ while(1):
             colavg = np.array([[np.mean(curpatch, axis=(0,1))]],dtype=np.uint8)
             hsvavg = cv2.cvtColor(colavg, cv2.COLOR_BGR2HSV)
             hue = float(hsvavg[0,0,0])
+            saturation = float(hsvavg[0,0,1])
             try:
                 matchidx = seenmatches.index(lastpoints[match.trainIdx])
             except:
@@ -55,26 +64,43 @@ while(1):
             if matchidx != -1:
                 matchstats[matchidx] += 1
                 seenmatches[matchidx] = keypoints[match.queryIdx]
-                trackedcolors[matchidx].append(hue)
+                trackedhue[matchidx].append(hue)
+                trackedsat[matchidx].append(saturation)
             else:
                 seenmatches.append(keypoints[match.queryIdx])
                 matchstats.append(2)
                 matchage.append(2)
-                trackedcolors.append([hue])
+                trackedhue.append([hue])
+                trackedsat.append([saturation])
         matchage = [age + 1 for age in matchage]
         for i in range(len(matchage))[::-1]:
             if matchage[i] > matchstats[i] + 1:
                 matchstats.pop(i)
                 seenmatches.pop(i)
                 matchage.pop(i)
-                trackedcolors.pop(i)
-        print(len(trackedcolors[0]))
+                trackedhue.pop(i)
+                trackedsat.pop(i)
+        print(len(trackedhue[0]))
         if showcameraview:
             blobs = cv2.drawKeypoints(img, seenmatches, blank, (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     elif showcameraview:
         blobs = cv2.drawKeypoints(img, keypoints, blank, (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     if showcameraview:
         cv2.imshow('image',blobs)
+        if showdemoview:
+            demoimg = cv2.subtract(demoimg, np.ones_like(demoimg) * decay_factor)
+            for i in range(len(trackedhue)):
+                dot = trackedhue[i]
+                sat = trackedsat[i]
+                if len(dot) > 5:
+                    lastpoint = (demorad + demorad * (sat[-2] / 255) * cos(radians(dot[-2]*2)),
+                        demorad + demorad * (sat[-2] / 255) * sin(radians(dot[-2]*2)))
+                    curpoint = (demorad + demorad * (sat[-1] / 255) * cos(radians(dot[-1]*2)),
+                        demorad + demorad * (sat[-1] / 255) * sin(radians(dot[-1]*2)))
+                    lastpoint = np.int32(lastpoint)
+                    curpoint = np.int32(curpoint)
+                    demoimg = cv2.line(demoimg, lastpoint, curpoint, democolors[i%3], 10)
+            cv2.imshow('demo',demoimg)
         x = cv2.waitKey(33)
         if x == 27:
             break
@@ -85,7 +111,7 @@ while(1):
 cap.release()
 cv2.destroyAllWindows()
 
-npcolors = np.array(trackedcolors)
+npcolors = np.array(trackedhue)
 
 for dot in npcolors:
     plt.plot(dot)
