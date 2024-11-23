679d72e8aa67893441692f236f71c0b060d8976d
ahpinder <42349096+ahpinder@users.noreply.github.com>
2024-11-23 15:07:04 -0500

    updated color parameters

--- ColorChange/ColorChange.ino
+++ ColorChange/ColorChange.ino
@@ -14,8 +14,10 @@
 #define LED_COUNT  9
 
 // NeoPixel brightness, 0 (min) to 255 (max)
-#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
+#define BRIGHTNESS 255 // Set BRIGHTNESS to about 1/5 (max = 255)
 // R - 0 G - 1 B - 1
+
+#define TIMEDELAY 50 // 50ms for 20Hz data rate
 
 //RGB enum for pattern
 enum Colors {
@@ -26,28 +28,29 @@ enum Colors {
 };
 
 // Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 uint32_t colors[] = {
-  strip.Color(255, 0, 0, 255), // Red
-  strip.Color(128, 255, 0, 255), // Green
-  strip.Color(0, 255, 255, 255), // Cyan
-  strip.Color(128, 0, 255, 255), // Purple
+  strip.Color(255, 0, 0), // Red
+  strip.Color(0, 255, 0), // Green
+  strip.Color(0, 20, 255), // Cyan
+  strip.Color(128, 0, 255), // Purple
 };
 
 uint32_t LEDS[9][5] = {
+  {R, G, C, G, R},
+  {R, G, C, C, R},
   {R, G, C, P, R},
-  {R, G, C, P, R},
-  {R, G, C, P, R},
-  {R, G, C, P, R},
-  {R, G, C, P, R},
-  {R, G, C, P, R},
-  {R, G, C, P, R},
-  {R, G, C, P, R},
-  {R, G, C, P, R},
+  {R, C, G, P, R},
+  {R, C, G, C, R},
+  {R, C, P, G, R},
+  {R, P, G, P, R},
+  {R, P, G, C, R},
+  {R, P, C, P, R},
 };
 
 bool firstRun = true;
+uint32_t lastlooptime;
 
 // Argument 1 = Number of pixels in NeoPixel strip
 // Argument 2 = Arduino pin number (most are valid)
@@ -69,6 +72,7 @@ void setup() {
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(BRIGHTNESS);
+  lastlooptime = millis();
 }
 
 void loop() {
@@ -78,7 +82,8 @@ void loop() {
         strip.setPixelColor(j, colors[LEDS[j][i]]);
         printf("Pixel %d : %d", j, colors[LEDS[j][i]]);
       }
-      delay(50); // Pause 1/20 second
+      while(millis() < lastlooptime + TIMEDELAY);
+      lastlooptime = millis();
       strip.show();
     }
   //    firstRun = false;
